8e3bd7b8a6ecd9dca02a1f555a142e2254b71a63
chaozy <chaozy.zhu.19@ucl.ac.uk>
2021-03-21 23:39:56 +0800

    shit

--- app/query_Handler.py
+++ app/query_Handler.py
@@ -6,7 +6,7 @@ pipe = None
 
 # The preprocessing of document is needed, which will assign values to pipe
 def processQuery(query):
-    if pipe is None:
-        raise Exception("Preprocessing of document is needed!")
+    # if pipe is None:
+    #     raise Exception("Preprocessing of document is needed!")
     queryResult = pipe.run(query=query, top_k_retriever=5, top_k_reader=3)
     return queryResult
